ac52f1a31d575714ecf39790d2976a09e8ee537e
kohanov <alexkohspb@gmail.com>
2017-05-17 10:21:13 +0300

    - update copyright

--- FiltersPlugin/GroupDialog.cpp
+++ FiltersPlugin/GroupDialog.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright “Renga Software” LLC, 2016. All rights reserved.
+// Copyright “Renga Software” LLC, 2017. All rights reserved.
 //
 // “Renga Software” LLC PROVIDES THIS PROGRAM "AS IS" AND WITH ALL FAULTS. 
 // “Renga Software” LLC  DOES NOT WARRANT THAT THE OPERATION OF THE PROGRAM WILL BE
